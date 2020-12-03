9601c7a6065d6d2d3c96ebac7c39474d9a19b96c
georgekhanachat <58150211+georgekhanachat@users.noreply.github.com>
2020-12-04 02:07:53 +0400

    Vector class completed
    Not commented yet

--- include/MVector.h
+++ include/MVector.h
@@ -1,11 +1,44 @@
 #ifndef INC_3DLIB_MVECTOR_H
 #define INC_3DLIB_MVECTOR_H
 
+#include <iostream>
+#include <cmath>
 
-class MVector
-{
+class MVector {
+    friend std::ostream &operator<<(std::ostream &os, MVector &obj);
 
+public:
+    // default constructor  Vector ve; XXX double check if the : lingo is to be done here or in definition
+    MVector() : x(0), y(0), z(0) {};
+
+    MVector(double x, double y, double z);
+
+    double getX() const;
+
+    double getY() const;
+
+    double getZ() const;
+
+    void setX(float x);
+
+    void setY(float y);
+
+    void setZ(float z);
+
+    ~MVector();
+
+    MVector operator+(MVector const &obj) const;
+
+    MVector operator-(MVector const &obj) const;
+
+    MVector operator*(MVector const &obj) const;
+
+    double getModulus() const;
+
+private:
+    double x;
+    double y;
+    double z;
 };
-
 
 #endif //INC_3DLIB_MVECTOR_H
--- lib/MVector.cpp
+++ lib/MVector.cpp
@@ -1,1 +1,67 @@
 #include "MVector.h"
+
+std::ostream &operator<<(std::ostream &os, MVector &obj) {
+    os << "x: " << obj.x << "  y: " << obj.y << "  z: " << obj.z << std::endl;
+    return os;
+}
+
+MVector::MVector(double x, double y, double z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+double MVector::getX() const {
+    return x;
+}
+
+double MVector::getY() const {
+    return y;
+}
+
+double MVector::getZ() const {
+    return z;
+}
+
+void MVector::setX(float x) {
+    this->x = x;
+}
+
+void MVector::setY(float y) {
+    this->y = z;
+}
+
+void MVector::setZ(float z) {
+    this->z = z;
+}
+
+double MVector::getModulus() const {
+    double mod = sqrt((x * x) + (y * y) + (z * z));
+    return mod;
+}
+
+MVector MVector::operator+(MVector const &obj) const {
+    MVector result;
+    result.x = x + obj.x;
+    result.y = y + obj.y;
+    result.z = z + obj.z;
+    return result;
+}
+
+MVector MVector::operator-(MVector const &obj) const {
+    MVector result;
+    result.x = x - obj.x;
+    result.y = y - obj.y;
+    result.z = z - obj.z;
+    return result;
+}
+
+MVector::~MVector() {}
+
+MVector MVector::operator*(MVector const &obj) const {
+    MVector result;
+    result.x = y * obj.z - z * obj.y;
+    result.y = z * obj.x - x * obj.z;
+    result.z = x * obj.y - y * obj.x;
+    return result;
+}
