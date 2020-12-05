c0022d111613f5d6829467e95f543942ec8ee580
georgekhanachat <58150211+georgekhanachat@users.noreply.github.com>
2020-12-05 23:39:29 +0400

    Commented header and cpp files
    MVector and Matrix are complete

--- lib/Matrix.cpp
+++ lib/Matrix.cpp
@@ -1,6 +1,8 @@
 #include "Matrix.h"
 
 std::ostream &operator<<(std::ostream &os, Matrix &obj) {
+    //Overloads << operator
+    // Produces an outstream to print the values of the matrix simultaneously in a matrix formation
     os << obj.mat3x3[0].getX() << "  " << obj.mat3x3[1].getX() << "  " << obj.mat3x3[2].getX() << std::endl;
     os << obj.mat3x3[0].getY() << "  " << obj.mat3x3[1].getY() << "  " << obj.mat3x3[2].getY() << std::endl;
     os << obj.mat3x3[0].getZ() << "  " << obj.mat3x3[1].getZ() << "  " << obj.mat3x3[2].getZ() << std::endl;
@@ -10,17 +12,30 @@ std::ostream &operator<<(std::ostream &os, Matrix &obj) {
 
 
 Matrix::Matrix(const MVector &v1, const MVector &v2, const MVector &v3) {
-
+    // Initializes matrix using 3 prepopulated vectors
     mat3x3.push_back(v1);
     mat3x3.push_back(v2);
     mat3x3.push_back(v3);
 
 }
 
-Matrix::~Matrix() {}
+// Default Constructor
+Matrix::~Matrix() = default;
 
 Matrix Matrix::operator+(const Matrix &obj) const {
+    // Matrix addition by looping through each vector in the matrix
+
+    /*
+     *     | 1   2   3|   | 1   2   3|   | a   b   c|
+     *     | 4   5   6| + | 4   5   6| = | d   e   f|
+     *     | 7   8   9|   | 7   8   9|   | g   h   i|
+     *
+     *     For Example:
+     *     a = 1 + 1 = 2
+     *     b = 2 + 2 = 4
+     */
     Matrix result;
+    // Loops through every vector
     for (int i = 0; i < 3; ++i) {
         result.mat3x3[i].setX(mat3x3[i].getX() + obj.mat3x3[i].getX());
         result.mat3x3[i].setY(mat3x3[i].getY() + obj.mat3x3[i].getY());
@@ -30,7 +45,20 @@ Matrix Matrix::operator+(const Matrix &obj) const {
 }
 
 Matrix Matrix::operator-(const Matrix &obj) const {
+    // Matrix subtraction by looping through each vector in the matrix
+
+    /*
+     *     | 1   2   3|   | 1   2   3|   | a   b   c|
+     *     | 4   5   6| - | 4   5   6| = | d   e   f|
+     *     | 7   8   9|   | 7   8   9|   | g   h   i|
+     *
+     *     For Example:
+     *     a = 1 - 1 = 0
+     *     b = 2 - 2 = 0
+     */
+
     Matrix result;
+    // Loops through every vector
     for (int i = 0; i < 3; ++i) {
         result.mat3x3[i].setX(mat3x3[i].getX() - obj.mat3x3[i].getX());
         result.mat3x3[i].setY(mat3x3[i].getY() - obj.mat3x3[i].getY());
@@ -40,7 +68,21 @@ Matrix Matrix::operator-(const Matrix &obj) const {
 }
 
 Matrix Matrix::operator*(const double &scalar) const {
+    // Matrix multiplication with a scalar
+    // by looping through each vector in the matrix
+    // and scaling it
+
+    /*
+     *     | 1   2   3|       | a   b   c|
+     *     | 4   5   6| * 2 = | d   e   f|
+     *     | 7   8   9|       | g   h   i|
+     *
+     *     For Example:
+     *     a = 2 * 1 = 2
+     */
+
     Matrix result;
+    // Loops through every vector
     for (int i = 0; i < 3; ++i) {
         result.mat3x3[i].setX(mat3x3[i].getX() * scalar);
         result.mat3x3[i].setY(mat3x3[i].getY() * scalar);
@@ -50,8 +92,24 @@ Matrix Matrix::operator*(const double &scalar) const {
 }
 
 Matrix Matrix::operator*(const Matrix &obj) const {
+    // Matrix multiplication with another matrix
+    // by looping through each vector in both matrices simultaneously
     Matrix result;
     double x1, x2, x3, y1, y2, y3, z1, z2, z3;
+
+    // This section has 3 parts to calculate the 3 components of each vector by
+    // multiplying each row but its respective column in the second matrix
+
+    /*
+     *     | 1   2   3|   | 1   2   3|   | a   b   c|
+     *     | 4   5   6| * | 4   5   6| = | d   e   f|
+     *     | 7   8   9|   | 7   8   9|   | g   h   i|
+     *
+     *     For Example:
+     *                        |1|
+     *     a = | 1   2   3| * |4| = 1*1 + 2*4 + 3*7 = 30
+     *                        |7|
+     */
 
     x1 = mat3x3[0].getX() * obj.mat3x3[0].getX() + mat3x3[1].getX() * obj.mat3x3[0].getY() +
          mat3x3[2].getX() * obj.mat3x3[0].getZ();
@@ -60,6 +118,7 @@ Matrix Matrix::operator*(const Matrix &obj) const {
     x3 = mat3x3[0].getX() * obj.mat3x3[2].getX() + mat3x3[1].getX() * obj.mat3x3[2].getY() +
          mat3x3[2].getX() * obj.mat3x3[2].getZ();
 
+    // Every set is then set using the setter in the MVector.h imported file
     result.mat3x3[0].setX(x1);
     result.mat3x3[1].setX(x2);
     result.mat3x3[2].setX(x3);
@@ -90,6 +149,20 @@ Matrix Matrix::operator*(const Matrix &obj) const {
 }
 
 MVector Matrix::operator*(const MVector &obj) const {
+    // Matrix multiplication with a vector
+    // by looping through each vector in the matrix
+
+    /*
+     *     | 1   2   3|   | 1 |   | a |
+     *     | 4   5   6| * | 4 | = | b |
+     *     | 7   8   9|   | 7 |   | c |
+     *
+     *     For Example:
+     *                        |1|
+     *     a = | 1   2   3| * |4| = 1*1 + 2*4 + 3*7 = 30
+     *                        |7|
+     */
+
     MVector result;
     double x = mat3x3[0].getX() * obj.getX() + mat3x3[1].getX() * obj.getY() + mat3x3[2].getX() * obj.getZ();
     double y = mat3x3[0].getY() * obj.getX() + mat3x3[1].getY() * obj.getY() + mat3x3[2].getY() * obj.getZ();
@@ -100,12 +173,17 @@ MVector Matrix::operator*(const MVector &obj) const {
     return result;
 }
 
-//double Matrix::det() const {
-//    //TODO
-//    return;
-//}
-
 Matrix Matrix::transponse() const {
+    // Transposing a matrix by reflecting the matrix of a diagonal
+
+    /*
+     *
+     *     | 1   2   3| T   | 1   4   7|
+     *     | 4   5   6|   = | 6   5   8|
+     *     | 7   8   9|     | 3   2   9|
+     *
+     */
+
     Matrix result;
     result.mat3x3[0].setX(mat3x3[0].getX());
     result.mat3x3[0].setY(mat3x3[1].getX());
@@ -120,6 +198,17 @@ Matrix Matrix::transponse() const {
 }
 
 double Matrix::det() const {
+    // Finding the determinant of a matrix to do various
+    // calculations like finding the inverse of the matrix
+
+    /*
+     *
+     *         | 1   2   3|         | 5  6 |       | 4  6 |       | 4  5 |
+     *     det | 4   5   6|   = 1 * | 8  9 | + 2 * | 7  9 | + 3 * | 7  8 | = 1(9*5-6*8) + 2(4*9-7*6)+ 3(4*8-5*7) = 0
+     *         | 7   8   9|
+     *
+     */
+
     double det;
     det = mat3x3[0].getX() * (mat3x3[1].getY() * mat3x3[2].getZ() - mat3x3[2].getY() * mat3x3[1].getZ()) -
           mat3x3[1].getX() * (mat3x3[0].getY() * mat3x3[2].getZ() - mat3x3[2].getY() * mat3x3[0].getZ()) +
@@ -128,10 +217,33 @@ double Matrix::det() const {
 }
 
 Matrix Matrix::inverse() const {
+    // Finding the inverse of a matrix through producing a cofactor matrix, transposing it
+    // and multiplying it by the inverse of the determinant of the input matrix
+
+    /*  To find the inverse you need to find the transposed cofactor matrix
+     *
+     *     | a   b   c| T   | a   h   g|
+     *     | d   e   f|   = | f   e   d|
+     *     | g   h   i|     | c   b   i|
+     *
+     *     Then to get the cofactor matrix, the determinant of each element needs to be found
+     *     The determinant is found if the lines that the specific element being looked at are erased
+     *     with only being left with a square of 4 elements.
+     *
+     *     For example:
+     *         | e  d |
+     *     a = | b  i | = ei - db
+     *
+     *        | a   h   g|   |           |
+     *        | f   e   d| = |    e    d |
+     *        | c   b   i|   |    b    i |
+     *
+     */
     Matrix cofactors, result, mat;
 
     mat.mat3x3 = this->mat3x3;
 
+    // Checks whether the determinant is zero because matrices with a det = 0 dont have an inverse
     if (mat.det() == 0) return result;
 
     cofactors.mat3x3[0].setX((mat3x3[1].getY() * mat3x3[2].getZ()) - (mat3x3[2].getY() * mat3x3[1].getZ()));
@@ -146,6 +258,7 @@ Matrix Matrix::inverse() const {
     cofactors.mat3x3[2].setY(-((mat3x3[0].getX() * mat3x3[1].getZ()) - (mat3x3[1].getX() * mat3x3[0].getZ())));
     cofactors.mat3x3[2].setZ((mat3x3[0].getX() * mat3x3[1].getY()) - (mat3x3[1].getX() * mat3x3[0].getY()));
 
+    // Calculation of the inverse
     result = cofactors.transponse() * (1 / mat.det());
     return result;
 }
