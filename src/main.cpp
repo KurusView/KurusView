a3fd25d9572e44d59af447c7674c219bdfc5a20f
georgekhanachat <58150211+georgekhanachat@users.noreply.github.com>
2020-12-09 01:25:54 +0400

    Proper structured testing function
    Commented and structured testing for vector and matrix classes

--- src/main.cpp
+++ src/main.cpp
@@ -34,32 +34,48 @@ void mVectorTest() {
     std::cout << "\n\nBegin MVector Test" << std::endl;
     std::cout << "---------------------------------------------------------------------------------------" << std::endl;
 
+    MVector addition, subtraction, vectorProd;
+
+    // Initializing 2 vectors to undergo tests
     MVector a(1, 2, 3);
-    MVector b(1, 2, 3);
-    MVector c(1, 2, 3);
-    MVector rot(pi/2, 0, 0);
-    MVector rot2(0, pi/4, 0);
+    MVector b(3, 2, 1);
+
+    std::cout << "Vector 1: " << std::endl;
+    std::cout << a;
+    std::cout << "Vector 2: " << std::endl;
+    std::cout << b;
+
+    // Initializing rotation vectors
+    MVector rot(pi / 2, 0, 0);
+    MVector rot2(0, pi / 4, 0);
     MVector rot3(0, 0, 1.23);
 
+    // Populating to input into rotation function
     std::vector<MVector> rotation;
     rotation.push_back(rot);
     rotation.push_back(rot2);
     rotation.push_back(rot3);
 
+    // Testing vector functions
+    addition = a + b;
+    subtraction = a - b;
+    vectorProd = a * b;
+
+    std::cout << "Addition: " << std::endl;
+    std::cout << addition;
+    std::cout << "Subtraction: " << std::endl;
+    std::cout << subtraction;
+    std::cout << "Vector Product: " << std::endl;
+    std::cout << vectorProd;
+    std::cout << "Modulus: " << a.getModulus() << std::endl;
+    std::cout << std::endl;
+
     a.rotate(rotation);
 
+    std::cout << "Rotated Vector: " << std::endl;
     std::cout << a;
 
-    MVector addition, subtraction, times;
-    addition = a + b + c;
-    subtraction = a - b - c;
-    times = a * b;
-    std::cout << addition << subtraction << times;
-
-    std::cout << "Mod: " << a.getModulus() << std::endl;
-
-    std::cout << "---------------------------------------------------------------------------------------"
-              << std::endl;
+    std::cout << "---------------------------------------------------------------------------------------" << std::endl;
     std::cout << "End MVector Test" << std::endl;
 }
 
@@ -93,44 +109,73 @@ void modelTest() {
 void matrixTest() {
     std::cout << "\n\nBegin Matrix Test" << std::endl;
     std::cout << "---------------------------------------------------------------------------------------" << std::endl;
-    MVector a(1, 4, 7);
-    MVector b(2, 5, 8);
-    MVector c(3, 6, 9);
-    MVector rot(pi / 2, 0, 0);
-    MVector rot2(0, 0, pi);
-    MVector rot3(pi, 0, 0);
-    MVector rot4(0, pi/3, 0);
+
+    // Initializing two matrices to undergo multiple tests
+    MVector a(1, 4, 7), b(2, 5, 8), c(3, 6, 9);
     Matrix m1(a, b, c);
-    Matrix m2(a, b, c);
-    MVector timesVec;
-    Matrix addition;
-    Matrix timesMat;
-    Matrix subtract;
-    Matrix times;
-    Matrix transpose;
-    Matrix inverse;
-    double det;
+    Matrix m2(c, b, a);
+    Matrix inverse(a, c, c);
+    Matrix transpose(a, b, c);
 
+    std::cout << "Matrix 1: " << std::endl;
+    std::cout << m1;
+    std::cout << "Matrix 2: " << std::endl;
+    std::cout << m2;
+    std::cout << "Initial inverse matrix: " << std::endl;
+    std::cout << inverse;
+
+    std::cout << "Initial transpose matrix: " << std::endl;
+    std::cout << inverse;
+
+    // Initializing matrix variables
+    Matrix subtraction, productMat, addition, productScalar;
+    MVector productVec;
+
+    //Testing matrix functions
     addition = m1 + m2;
-    subtract = m1 - m2;
-    times = m1 * 2.53421;
-    timesVec = m1 * a;
-    timesMat = m1 * m2;
-    transpose = m1.transponse();
-    det = m1.det();
-    inverse = m1.inverse();
+    subtraction = m1 - m2;
+    productMat = m1 * m2;
+    productScalar = m1 * pi;
+    productVec = m1 * a;
 
-    std::cout << addition << subtract << times << timesVec << timesMat;
-    std::cout << transpose << inverse;
-    std::cout << det << std::endl;
+    std::cout << "Addition: " << std::endl;
+    std::cout << addition;
+    std::cout << "Subtraction: " << std::endl;
+    std::cout << subtraction;
+    std::cout << "Matrix multiplication: " << std::endl;
+    std::cout << productMat;
+    std::cout << "Matrix-Vector multiplication: " << std::endl;
+    std::cout << productVec;
+    std::cout << "Matrix-Vector multiplication: " << std::endl;
+    std::cout << productVec;
+    std::cout << "Determinant: " << m1.det() << std::endl;
     std::cout << std::endl;
+
+    // Initializing rotation vectors
+    MVector rot(pi / 2, 0, 0);
+    MVector rot2(0, pi / 4, 0);
+    MVector rot3(0, 0, 1.23);
+
+    // Populating to input into rotation function
     std::vector<MVector> rotation;
     rotation.push_back(rot);
     rotation.push_back(rot2);
     rotation.push_back(rot3);
-    rotation.push_back(rot4);
+
     m1.rotate(rotation);
+
+    std::cout << "Rotated Matrix m1: " << std::endl;
     std::cout << m1;
+
+    inverse.inverse();
+
+    std::cout << "Inverse of Matrix: " << std::endl;
+    std::cout << inverse;
+
+    transpose.Transponse();
+
+    std::cout << "Transpose of Matrix: " << std::endl;
+    std::cout << transpose;
 
     std::cout << "---------------------------------------------------------------------------------------" << std::endl;
     std::cout << "End Matrix Test" << std::endl;
