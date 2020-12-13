0c93cfb51fa0d5e4d5e975f93cd8736e05fd97f4
zadewg <zadewg@gmail.com>
2020-12-13 18:37:46 +0000

    add setters, getters

--- lib/MCell.cpp
+++ lib/MCell.cpp
@@ -9,3 +9,49 @@ MCell::MCell(std::vector<MVector> vertices, Material material, const long int id
 MCell::~MCell() {
     MCellInstanceCount--;
 }
+
+const double MCell::getVolume(void) const {
+    return this->MCellVolume;
+}
+
+const double MCell::getWeight(void) const {
+    return this->MCellWeight;
+}
+
+const double MCell::getMass(void) const {
+    return this->MCellMass;
+}
+
+const double MCell::getDensity(void) const {
+    return this->MCellDensity;
+}
+
+const double MCell::getID(void) const {
+    return this->MCellID;
+}
+
+const MVector MCell::getCentreOfGrav(void) {
+    return this->MCellCOG;
+}
+
+const Material MCell::getMaterial() {
+    return this->MCellMaterial;
+}
+
+const std::vector<MVector> MCell::getVertices(void) {
+    return this->MCellVertices;
+}
+
+const double MCell::getCount(void) const {
+    return this->MCellInstanceCount;
+}
+
+
+void MCell::setMaterial(Material material) {
+    this->MCellMaterial = material;
+}
+
+void MCell::setVertices(std::vector<MVector> vertices) {
+    this->MCellVertices = vertices;
+}
+
