d269f40d4b73957731171f7d538114f923cca04d
zadewg <zadewg@gmail.com>
2020-12-13 11:59:02 +0000

    Mcell initial commit

--- include/MCell.h
+++ include/MCell.h
@@ -1,9 +1,85 @@
 #ifndef INC_3DLIB_MCELL_H
 #define INC_3DLIB_MCELL_H
 
+#include <iostream>
+#include <vector>
+
+#include "MVector.h"
+#include "Material.h"
 
 class MCell {
+private:
 
+    // Data Members
+
+    double MCellVolume;
+    double MCellWeight;
+    double MCellMass;
+    double MCellDensity;
+    double MCellID;
+
+    // color? - check with Material
+
+    std::vector<MVector> MCellVertices; // shared ptr
+    Material MCellMaterial;             // shared ptr
+
+protected:
+
+    // note: when mapping enum to string for printing, make it Compile Time Evaluated
+    // (not std::map), maybe constexpr: https://stackoverflow.com/a/63265237
+    // If the above becomes a headache make this any other POD
+    enum class _MCellType_t{
+        NONE,
+        TETRAHEDRON,
+        HEXAHEDRON,
+        PYRAMID,
+    };
+
+    _MCellType_t MCellType = _MCellType_t::NONE; // do this in constructor member initializer list?
+
+    // require shape specific knowledge and are to be implemented by derived classes (virtual)
+    virtual double calcVolume( void );
+    virtual double calcWeight( void );
+    virtual MVector calcCentreOfGrav ( void );
+
+
+public:
+
+    // ================ CONSTRUCTORS/DESTRUCTORS ================
+    MCell( std::vector<MVector> _MCellVertices, Material _MCellMaterial, double _MCellID); // shared_ptr material, MVector.
+    ~MCell() = default;
+
+    // ====================== OPERATORS ==========================
+    MCell& operator=( const MCell& _mcell );
+
+    // stdout, to file streams
+    friend std::ostream &operator<<( std::ostream &os, const MCell& _MCell );
+    friend std::ofstream &operator<<( std::ofstream &os, const MCell& _MCell );
+
+    // ======================= ACCESSORS =========================
+
+    // XXX: a note on const: https://stackoverflow.com/questions/8406898/benefits-of-using-const-with-scalar-type-e-g-const-double-or-const-int
+    double getVolume( void );
+    double getWeight( void );
+    double getMass( void );
+    double getDensity( void );
+    double getMCellID ( void );
+
+    std::vector<std::string> getMCellType ( void ); // eg. t, tetrahedron (for file and stdout output)
+
+    int setMCellID ( double MCellID ); // fail on repeated ID?
+
+
+    // for now return instance copy of these. https://en.wikipedia.org/wiki/Copy_elision#Return_value_optimization
+    // best implemented by reference, use smart pointers:
+    const MVector getCentreOfGrav ( void ); // unique in 3D space?
+    const Material getMaterial( void ); // shared
+    const std::vector<MVector> getVertices( void ); // MVector shared, std::vector: http://www.cplusplus.com/forum/general/56177/
+
+    void setMaterial( Material ); // shared
+    void setVertices ( std::vector<MVector> ); // MVector shared
+
+    // ==============================================================
 };
 
 
