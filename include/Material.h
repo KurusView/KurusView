66f60e2ca37de51f5416120a2a853dfe71d7469a
YusufTizani <eeyyt4@nottingham.ac.uk>
2020-12-15 15:15:53 +0000

    Add material class

--- include/Material.h
+++ include/Material.h
@@ -1,10 +1,35 @@
 #ifndef INC_3DLIB_MATERIAL_H
 #define INC_3DLIB_MATERIAL_H
 
+#include <string>
 
+// Class for the Material and its properties.
 class Material {
+private:
+    const unsigned int ID; // Object for the material ID.
+    std::string name; // Object of the material name.
+    std::string colour; // Object for the material colour.
+    double density; // Object for the material density.
+public:
+    Material();
+    Material(std::string name, std::string colour, double density, unsigned int id);
+
+    ~Material() = default;
+
+    unsigned int getId() const;
+
+    std::string getName() const;
+
+    void setName(const std::string name);
+
+    std::string getColour() const;
+
+    void setColour(const std::string colour);
+
+    double getDensity() const;
+
+    void setDensity(double density);
 
 };
-
 
 #endif //INC_3DLIB_MATERIAL_H
