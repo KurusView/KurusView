0dfdc687c8ca6450ab51bd3ce8addfd67ae6eeda
Omar Abouelleil <36559888+oabouelleil@users.noreply.github.com>
2020-12-21 14:20:06 +0400

    Update scale

--- lib/ModelRenderer.cpp
+++ lib/ModelRenderer.cpp
@@ -20,7 +20,7 @@ void ModelRenderer::run() {
 
     Mesh mesh(model);
 
-    glm::vec3 m_scale = {80, 80, 80};
+    glm::vec3 m_scale = {1, 1, 1};
     float m_rotate = 1.55;
 
     glm::mat4 transform;
