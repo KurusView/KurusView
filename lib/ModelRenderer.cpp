e5368416b869e84f8019509e0a1b95bfb8895ab8
Omar Abouelleil <36559888+oabouelleil@users.noreply.github.com>
2021-05-05 20:46:13 +0400

    Scale splash screen to screen size

--- lib/ModelRenderer.cpp
+++ lib/ModelRenderer.cpp
@@ -2,12 +2,18 @@
 
 #include <QSplashScreen>
 #include <QSurfaceFormat>
+#include <QScreen>
 #include <QVTKOpenGLWidget.h>
 
 ModelRenderer::ModelRenderer(int &argc, char **argv) : QApplication(argc, argv), window(), welcomeWindow() {
-    QPixmap splashImage(":/Icons/splashscreen.png");
-    QSplashScreen splashScreen(splashImage);
+    // Get Primary Screen Height
+    int screenHeight = QGuiApplication::primaryScreen()->geometry().height();
+    // Load in the splash screen image and scale it to 1/3 of the screen height, keeping the same aspect ratio
+    QPixmap splashImage = QPixmap(":/Icons/splashscreen.png").scaledToHeight(screenHeight / 3);
+    // Create and show the splash screen, keeping it on top
+    QSplashScreen splashScreen(splashImage, Qt::WindowStaysOnTopHint);
     splashScreen.show();
+
     // needed to ensure appropriate OpenGL context is created for VTK rendering.
     QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
 
