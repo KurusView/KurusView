f556bbaa33a2ee3430a09cb05157186f873f0dc6
Omar Abouelleil <36559888+oabouelleil@users.noreply.github.com>
2021-05-05 15:44:29 +0400

    Fix Splash Screen and main window icon

--- lib/ModelRenderer.cpp
+++ lib/ModelRenderer.cpp
@@ -1,9 +1,13 @@
 #include "ModelRenderer.h"
 
+#include <QSplashScreen>
 #include <QSurfaceFormat>
 #include <QVTKOpenGLWidget.h>
 
 ModelRenderer::ModelRenderer(int &argc, char **argv) : QApplication(argc, argv), window(), welcomeWindow() {
+    QPixmap splashImage(":/Icons/logo.png");
+    QSplashScreen splashScreen(splashImage);
+    splashScreen.show();
     // needed to ensure appropriate OpenGL context is created for VTK rendering.
     QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
 
