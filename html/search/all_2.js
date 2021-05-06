ea18c7ad44c863f2ed15cc56d8668b4e38ce5954
Travis CI <travis@travis-ci.com>
2021-05-06 14:57:31 +0000

    Deploy docs: 4
    Commit: 2298f329a82f077cc19a41d60949d198dcbaca0d

---
+++ html/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['team_20members_20and_20roles',['Team Members and Roles',['../md__home_travis_build_KurusView_2020_GROUP_21_TEAM.html',1,'']]]
+];
---
+++ html/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['news',['NEWS',['../md__home_travis_build_KurusView_2020_GROUP_21_NEWS.html',1,'']]]
+];
