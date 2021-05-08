4697b02f502584f56aa05ec178a20fe1cc13a938
Travis CI <travis@travis-ci.com>
2021-05-08 14:13:57 +0000

    Deploy docs: 23
    Commit: 80680a7be0e29ab5091b02d21cf1767ba4ad6587

---
+++ html/dir_2d1d991c66e7a032b2ab80c11328e79c.js
@@ -0,0 +1,4 @@
+var dir_2d1d991c66e7a032b2ab80c11328e79c =
+[
+    [ "dark_mode_detect.h", "dark__mode__detect_8h_source.html", null ]
+];
--- html/dir_d44c64559bbebec7f509842c48db8b23.js
+++ html/dir_d44c64559bbebec7f509842c48db8b23.js
@@ -2,6 +2,7 @@ var dir_d44c64559bbebec7f509842c48db8b23 =
 [
     [ "Cells", "dir_1ac8995046f5d69bd22d51310bd60508.html", "dir_1ac8995046f5d69bd22d51310bd60508" ],
     [ "ModelRenderer", "dir_7c86cc7646f5055c93232a0a5d7bcb7d.html", "dir_7c86cc7646f5055c93232a0a5d7bcb7d" ],
+    [ "Utils", "dir_2d1d991c66e7a032b2ab80c11328e79c.html", "dir_2d1d991c66e7a032b2ab80c11328e79c" ],
     [ "Material.h", "_material_8h_source.html", null ],
     [ "Matrix.h", "_matrix_8h.html", "_matrix_8h" ],
     [ "MCell.h", "_m_cell_8h.html", [
