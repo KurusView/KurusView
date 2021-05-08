adff18e7cd6342c259f96ffcafdb1c14b7a689c0
zadewg <zadewg@gmail.com>
2021-05-08 19:49:10 +0200

    Update color codes

--- lib/ModelRenderer.cpp
+++ lib/ModelRenderer.cpp
@@ -10,6 +10,8 @@
 
 #include "Utils/dark_mode_detect.h"
 
+#define USE_FUSION
+
 ModelRenderer::ModelRenderer(int &argc, char **argv) : QApplication(argc, argv), welcomeWindow() {
     // Get Primary Screen Height
     int screenHeight = QGuiApplication::primaryScreen()->geometry().height();
@@ -22,44 +24,55 @@ ModelRenderer::ModelRenderer(int &argc, char **argv) : QApplication(argc, argv),
     // needed to ensure appropriate OpenGL context is created for VTK rendering.
     QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
 
-//    // Open Stylesheet file
-//    QFile styleSheetFile(":/Icons/Combinear.qss");
-//    styleSheetFile.open(QFile::ReadOnly);
-//    // Store all the contents of the file in a string
-//    QString styleSheet = QLatin1String(styleSheetFile.readAll());
-//    // Load the stylesheet
-//    setStyleSheet(styleSheet);
+#ifndef USE_FUSION
+    // Open Stylesheet file
+    QFile styleSheetFile(":/Icons/Combinear.qss");
+    styleSheetFile.open(QFile::ReadOnly);
+    // Store all the contents of the file in a string
+    QString styleSheet = QLatin1String(styleSheetFile.readAll());
+    // Load the stylesheet
+    setStyleSheet(styleSheet);
+#endif
 
-    QApplication::setStyle(QStyleFactory::create("Fusion"));
-
-    DarkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+    // palette pulled heavily from https://github.com/Jorgen-VikingGod/Qt-Frameless-Window-DarkStyle
+    DarkPalette.setColor(QPalette::Window, QColor("#353535"));
     DarkPalette.setColor(QPalette::WindowText, Qt::white);
-    DarkPalette.setColor(QPalette::Base, QColor(15, 15, 15));
-    DarkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+    DarkPalette.setColor(QPalette::Base, QColor("#2a2a2a"));
+    DarkPalette.setColor(QPalette::AlternateBase, QColor("#424242"));
     DarkPalette.setColor(QPalette::ToolTipBase, Qt::white);
-    DarkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    DarkPalette.setColor(QPalette::ToolTipText, QColor("#353535"));
     DarkPalette.setColor(QPalette::Text, Qt::white);
-    DarkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    DarkPalette.setColor(QPalette::Dark, QColor("#232323"));
+    DarkPalette.setColor(QPalette::Shadow, QColor("#141414"));
+    DarkPalette.setColor(QPalette::Button, QColor("#353535"));
     DarkPalette.setColor(QPalette::ButtonText, Qt::white);
     DarkPalette.setColor(QPalette::BrightText, Qt::red);
-
-    DarkPalette.setColor(QPalette::Highlight, QColor(142, 45, 197).lighter());
-    DarkPalette.setColor(QPalette::HighlightedText, Qt::black);
+    DarkPalette.setColor(QPalette::Link, QColor("#2a82da"));
+    DarkPalette.setColor(QPalette::Highlight, QColor("#2a82da"));
+    DarkPalette.setColor(QPalette::HighlightedText, Qt::white);
 
     // rules for disabled texts
-    DarkPalette.setColor(QPalette::Disabled, QPalette::Text, Qt::darkGray);
-    DarkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, Qt::darkGray);
+    DarkPalette.setColor(QPalette::Disabled, QPalette::WindowText, QColor("#7f7f7f"));
+    DarkPalette.setColor(QPalette::Disabled, QPalette::Text, QColor("#7f7f7f"));
+    DarkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, QColor("#7f7f7f"));
+    DarkPalette.setColor(QPalette::Disabled, QPalette::Highlight, QColor("#505050"));
+    DarkPalette.setColor(QPalette::Disabled, QPalette::HighlightedText, QColor("#7f7f7f"));
 
-    // apply mode on start-up
-    applyLightMode();
 
     // configure PIT
     auto *activeTimer = new QTimer(this);
     activeTimer->setInterval(2 * 1000); //5 seconds
     activeTimer->setSingleShot(false);
 
+#ifdef USE_FUSION
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
+
+    // apply mode on start-up
+    applyLightMode();
+
     // connect timer signal to slot
     connect(activeTimer, SIGNAL(timeout()), this, SLOT(applyLightMode()));
+#endif
 
     // start timer - can be disabled through stop(). Useful if settings forces light/dark mode.
     activeTimer->start();
