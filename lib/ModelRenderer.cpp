801cb43841c9ddef2ebfde610069bd1a27559a60
zadewg <zadewg@gmail.com>
2021-05-08 14:48:18 +0200

    Dark mode support
    Colors are to be tweaked

--- lib/ModelRenderer.cpp
+++ lib/ModelRenderer.cpp
@@ -30,6 +30,32 @@ ModelRenderer::ModelRenderer(int &argc, char **argv) : QApplication(argc, argv),
 
     QApplication::setStyle(QStyleFactory::create("Fusion"));
 
+    QPalette palette;
+    palette.setColor(QPalette::Window, QColor(53, 53, 53));
+    palette.setColor(QPalette::WindowText, Qt::white);
+    palette.setColor(QPalette::Base, QColor(15, 15, 15));
+    palette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+    palette.setColor(QPalette::ToolTipBase, Qt::white);
+    palette.setColor(QPalette::ToolTipText, Qt::white);
+    palette.setColor(QPalette::Text, Qt::white);
+    palette.setColor(QPalette::Button, QColor(53, 53, 53));
+    palette.setColor(QPalette::ButtonText, Qt::white);
+    palette.setColor(QPalette::BrightText, Qt::red);
+
+    palette.setColor(QPalette::Highlight, QColor(142, 45, 197).lighter());
+    palette.setColor(QPalette::HighlightedText, Qt::black);
+
+    // rules for disabled texts
+    palette.setColor(QPalette::Disabled, QPalette::Text, Qt::darkGray);
+    palette.setColor(QPalette::Disabled, QPalette::ButtonText, Qt::darkGray);
+
+    // enable dark mode
+    QApplication::setPalette(palette);
+
+    // switch back to light mode
+//    QPalette palletblank;
+//    QApplication::setPalette(palletblank);
+
     // TODO Check if a path was given as an argument
     //  If so, then directly load up a model window with that model
     //  Otherwise, show the welcome window, which will then load the
