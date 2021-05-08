820e97e0c5b69a0dd98d1f1a207739834e97131f
Travis CI <travis@travis-ci.com>
2021-05-08 11:13:07 +0000

    Deploy docs: 16
    Commit: 95b549e6f23afb9a6e6e3e288e5b295e4777f3d4

--- html/class_model.js
+++ html/class_model.js
@@ -7,7 +7,8 @@ var class_model =
     [ "calcCentre", "class_model.html#a684fce1b8bf9d129bd03d6375a2a7c7f", null ],
     [ "displayCells", "class_model.html#aadf1269e558e3581fe58fe47ad9b65e7", null ],
     [ "displayVertexCount", "class_model.html#abea7440824f0c1cd58e523f08c486ca6", null ],
-    [ "getVTKModel", "class_model.html#aa06a3d1d6a975f23cfaa9e57e18d1a47", null ],
+    [ "getVTKModel", "class_model.html#ab63cf37f9d2b24e07c01f3bf0e8653f6", null ],
     [ "loadModel", "class_model.html#a1b24f12f7610a5ebfa8e61c322e3952b", null ],
+    [ "loadSTLModel", "class_model.html#a707d660adfc089994e11c158172d42d2", null ],
     [ "saveModel", "class_model.html#a41873a62770f294e288f78f908d8868d", null ]
 ];
