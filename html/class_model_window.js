94d9fc5c1555b6fe880d0bcabf22a2d961fa900b
Travis CI <travis@travis-ci.com>
2021-05-08 21:47:10 +0000

    Deploy docs: 34
    Commit: 179816f826d1057e7beaa1bebb163dc0f48a223c

--- html/class_model_window.js
+++ html/class_model_window.js
@@ -4,9 +4,8 @@ var class_model_window =
     [ "~ModelWindow", "class_model_window.html#a0921e8764c58766b4a9bf8023e9f035e", null ],
     [ "handleBackgroundColor", "class_model_window.html#adfe2628591eb1f94d52a12f4d30444e0", null ],
     [ "handleLightIntensitySlider", "class_model_window.html#a8d2a92772f793db843b5b0f9b4efb4db", null ],
-    [ "handleLightOpacitySlider", "class_model_window.html#a037ea927ed232fc5b7f668b602b55c4f", null ],
-    [ "handleLightSpecularitySlider", "class_model_window.html#a7916ea8b24e1f7c9c794a3f2d9e187b6", null ],
     [ "handleModelColor", "class_model_window.html#acc1f27a271177a4449ee2c5f042b41b3", null ],
     [ "handleResetColor", "class_model_window.html#a4c9eefb90446f87941efa952e6b29900", null ],
-    [ "handleResetLighting", "class_model_window.html#a2e42c0dabaa52b19487e8bf58b337397", null ]
+    [ "handleResetLighting", "class_model_window.html#a2e42c0dabaa52b19487e8bf58b337397", null ],
+    [ "mux_handleLightActorSlider", "class_model_window.html#a56afb845e27afd543a4a0a1f9bcebc57", null ]
 ];
