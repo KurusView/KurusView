97a5ce4790d215d18a0e5ca654902ae3e15f3dad
Travis CI <travis@travis-ci.com>
2021-05-08 20:29:37 +0000

    Deploy docs: 28
    Commit: 4830471ec42fcdda9da9ea6fa9211e667793170e

--- html/class_model_window.js
+++ html/class_model_window.js
@@ -1,5 +1,7 @@
 var class_model_window =
 [
     [ "ModelWindow", "class_model_window.html#a1319e984ea119dd39c2d4a53348550a4", null ],
-    [ "~ModelWindow", "class_model_window.html#a0921e8764c58766b4a9bf8023e9f035e", null ]
+    [ "~ModelWindow", "class_model_window.html#a0921e8764c58766b4a9bf8023e9f035e", null ],
+    [ "handleBackgroundColor", "class_model_window.html#adfe2628591eb1f94d52a12f4d30444e0", null ],
+    [ "handleModelColor", "class_model_window.html#acc1f27a271177a4449ee2c5f042b41b3", null ]
 ];
