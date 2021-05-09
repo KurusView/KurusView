a461cfbcd476dfc5f8e5d1f49e6107f6d257710b
Travis CI <travis@travis-ci.com>
2021-05-09 13:05:19 +0000

    Deploy docs: 37
    Commit: 51624fdb35a273c3d0c4b400a44a196ffee721ae

--- html/class_model_window.js
+++ html/class_model_window.js
@@ -2,13 +2,16 @@ var class_model_window =
 [
     [ "ModelWindow", "class_model_window.html#a1319e984ea119dd39c2d4a53348550a4", null ],
     [ "~ModelWindow", "class_model_window.html#a0921e8764c58766b4a9bf8023e9f035e", null ],
+    [ "gridlinesInit", "class_model_window.html#a6747b53911f566036be4b1376e9bd92c", null ],
     [ "handleBackgroundColor", "class_model_window.html#adfe2628591eb1f94d52a12f4d30444e0", null ],
     [ "handleChangePerspective", "class_model_window.html#a347aaf495ab87259cf021097ccd7e486", null ],
+    [ "handleGridlines", "class_model_window.html#a0be0aad8e3a69aad988419dbd013b983", null ],
     [ "handleLightIntensitySlider", "class_model_window.html#a8d2a92772f793db843b5b0f9b4efb4db", null ],
     [ "handleModelColor", "class_model_window.html#acc1f27a271177a4449ee2c5f042b41b3", null ],
     [ "handleResetColor", "class_model_window.html#a4c9eefb90446f87941efa952e6b29900", null ],
     [ "handleResetLighting", "class_model_window.html#a2e42c0dabaa52b19487e8bf58b337397", null ],
     [ "mux_handleLightActorSlider", "class_model_window.html#a56afb845e27afd543a4a0a1f9bcebc57", null ],
+    [ "toggleGridlines", "class_model_window.html#ae303729d7b06c5ec12e5cd07d7218559", null ],
     [ "updateFilters", "class_model_window.html#a274d01769538d149595a5cce505c07b5", null ],
     [ "updateStructure", "class_model_window.html#a22662895af406e3889535120c7bb748b", null ]
 ];
