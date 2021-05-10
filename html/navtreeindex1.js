ed30d40b03780903d36fdd8e5468553fc93ec007
Travis CI <travis@travis-ci.com>
2021-05-10 21:05:49 +0000

    Deploy docs: 69
    Commit: 57c0726fa4869d14c4ba176fb2b1bc7904a1f242

--- html/navtreeindex1.js
+++ html/navtreeindex1.js
@@ -1,5 +1,8 @@
 var NAVTREEINDEX1 =
 {
+"md__home_travis_build_KurusView_2020_GROUP_21_TEAM.html":[2],
+"modelwindow_8h_source.html":[4,0,0,1,1],
+"pages.html":[],
 "settingsdialog_8h_source.html":[4,0,0,1,2],
 "welcomewindow_8h_source.html":[4,0,0,1,4]
 };
