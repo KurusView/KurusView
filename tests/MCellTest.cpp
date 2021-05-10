ab66b3c38cb2575f4c1a8b3a642ccfc706d87be9
georgekhanachat <58150211+georgekhanachat@users.noreply.github.com>
2021-05-11 00:43:44 +0400

    Test MCell functions

--- tests/MCellTest.cpp
+++ tests/MCellTest.cpp
@@ -4,6 +4,7 @@
 #include "MTetrahedron.h"
 #include "MPyramid.h"
 #include "MHexahedron.h"
+#include "MVector.h"
 
 #include "Material.h"
 
@@ -13,7 +14,7 @@ std::shared_ptr<MVector> SharedVector = std::make_shared<MVector>(1, 2, 3, 99);
 // Define a Material
 std::shared_ptr<Material> SharedMaterial = std::make_shared<Material>("testM",
                                                                       "FFFF",
-                                                                      123456,
+                                                                      8940,
                                                                       11);
 
 // create vector of vertexes
@@ -24,7 +25,7 @@ std::vector<std::shared_ptr<MVector>> pyra_vertex = {SharedVector, SharedVector,
                                                      SharedVector, SharedVector, SharedVector};
 
 std::vector<std::shared_ptr<MVector>> hexa_vertex = {SharedVector, SharedVector, SharedVector, SharedVector,
-                                                     SharedVector, SharedVector, SharedVector, SharedVector };
+                                                     SharedVector, SharedVector, SharedVector, SharedVector};
 
 // create some cells to increase the instance count (instance count = 3)
 MTetrahedron tetra(tetra_vertex, SharedMaterial, 0);
@@ -58,7 +59,27 @@ TEST(MCellTest, id) {
     ASSERT_TRUE(tetra.getID() == 0);
 }
 
+TEST(MCellTest, getVertices) {
+    std::vector<std::shared_ptr<MVector>> vertices;
+    std::shared_ptr<MVector> v1 = std::make_shared<MVector>(0.0, 0.0, 0.0);
+    std::shared_ptr<MVector> v2 = std::make_shared<MVector>(0.0, -2.0, 0.0);
+    std::shared_ptr<MVector> v3 = std::make_shared<MVector>(2.0, -2.0, 0.0);
+    std::shared_ptr<MVector> v4 = std::make_shared<MVector>(2.0, 0.0, 0.0);
+    std::shared_ptr<MVector> v5 = std::make_shared<MVector>(1.0, -1.0, 2.0);
+    vertices.push_back(v1);
+    vertices.push_back(v2);
+    vertices.push_back(v3);
+    vertices.push_back(v4);
+    vertices.push_back(v5);
 
+    MPyramid pyramid(vertices, SharedMaterial, 0);
+
+    std::vector<std::shared_ptr<MVector>> verticesObtained = pyramid.getVertices();
+
+    for (int i = 0; i < vertices.size(); i++) {
+        ASSERT_EQ(vertices[i], verticesObtained[i]);
+    }
+}
 
 
 
