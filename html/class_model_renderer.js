3a5eede81de7ee22b17f18ed7e5aa784d408651a
Travis CI <travis@travis-ci.com>
2021-05-10 09:22:06 +0000

    Deploy docs: 48
    Commit: 5f54491eed1d011443205e2d750686ceac7f5bcf

--- html/class_model_renderer.js
+++ html/class_model_renderer.js
@@ -1,5 +1,6 @@
 var class_model_renderer =
 [
     [ "ModelRenderer", "class_model_renderer.html#a26b34d370db1a593c9a6c53ea155567d", null ],
-    [ "applyLightMode", "class_model_renderer.html#a5ddff3a1978e30ebe53af738e284cf67", null ]
+    [ "applyLightMode", "class_model_renderer.html#a5ddff3a1978e30ebe53af738e284cf67", null ],
+    [ "openFile", "class_model_renderer.html#aeedd14eaac0d49766eb02c90c9852eca", null ]
 ];
--- html/class_welcome_window.js
+++ html/class_welcome_window.js
@@ -4,6 +4,7 @@ var class_welcome_window =
     [ "~WelcomeWindow", "class_welcome_window.html#a19e92915de4238d9cebfc94c6ff0ed28", null ],
     [ "addToRecentFiles", "class_welcome_window.html#a6faa13607aacf9dbb2fff495144dd989", null ],
     [ "eventFilter", "class_welcome_window.html#ac81cf522736bdceab78127dd62f6dc2f", null ],
+    [ "fileSelected", "class_welcome_window.html#a11807b4207825be48a697c6942227320", null ],
     [ "handleAboutButton", "class_welcome_window.html#a35d5fdd96f2592b991c142d1fc3e0d02", null ],
     [ "handleOpenButton", "class_welcome_window.html#a6df5805ce1c6d7bd0d59a71ed13a0a4b", null ],
     [ "removeEntryFromRecents", "class_welcome_window.html#af6474706e21f5301800f916bad14ee5f", null ]
