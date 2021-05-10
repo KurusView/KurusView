9c4011d63677222624c5fa1deb5ef6a4353a0096
Travis CI <travis@travis-ci.com>
2021-05-10 12:51:17 +0000

    Deploy docs: 54
    Commit: 88228a301c39ca7873d850861c41b95b2712bc8c

--- html/class_welcome_window.js
+++ html/class_welcome_window.js
@@ -3,6 +3,7 @@ var class_welcome_window =
     [ "WelcomeWindow", "class_welcome_window.html#a7a3e2e0f8890a94a985b7969f2ed102d", null ],
     [ "~WelcomeWindow", "class_welcome_window.html#a19e92915de4238d9cebfc94c6ff0ed28", null ],
     [ "addToRecentFiles", "class_welcome_window.html#a291ff8588743fb2db5c7525f2af28502", null ],
+    [ "copyEntryToClipboard", "class_welcome_window.html#a8aff3661e9f490cf54119a0ee4300b69", null ],
     [ "eventFilter", "class_welcome_window.html#ac81cf522736bdceab78127dd62f6dc2f", null ],
     [ "fileSelected", "class_welcome_window.html#a692091e3e07a53b8b929100648ce0573", null ],
     [ "handleAboutButton", "class_welcome_window.html#a35d5fdd96f2592b991c142d1fc3e0d02", null ],
