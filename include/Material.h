d5f7f30b1c6e84fd7e5ddf95d56594f7ecfa81cc
YusufTizani <eeyyt4@nottingham.ac.uk>
2021-05-11 15:12:40 +0100

    Add documentation to Material.h

--- include/Material.h
+++ include/Material.h
@@ -12,25 +12,67 @@ private:
     std::string colour; // Object for the material colour.
     double density; // Object for the material density.
 public:
+    /**
+     *
+     */
     Material();
+    /**
+     * @brief Material - Sets the propeties of the material in the model.
+     * @param name - The name of the material
+     * @param colour - Colour of the material
+     * @param density - The density of the material
+     * @param id  - The id of the material
+     */
     Material(std::string name, std::string colour, double density, unsigned int id);
-
+    /*
+     * @breif ~Material - Destructor
+     */
     ~Material() = default;
 
+    /**
+     * @breif
+     * @param os
+     * @param obj
+     * @return
+     */
     friend std::ofstream &operator<<(std::ofstream &os, Material &obj);
 
     unsigned int getId() const;
 
+    /**
+     * @brief getName - Gets the name of the material
+     * @return name- The name of the material
+     */
     std::string getName() const;
 
+    /**
+     * @breif setName - Sets the name of the material
+     * @param name - The name of the material set
+     */
     void setName(const std::string name);
 
+    /**
+     * @brief getColour - Gets the colour of the material
+     * @return colour - The colour of the material
+     */
     std::string getColour() const;
 
+    /**
+     * @brief setColour - Sets the colour of the material
+     * @param colour  - The colour set for the material
+     */
     void setColour(const std::string colour);
 
+    /**
+     * @breif getDensity - Gets the density of the material
+     * @return density - The density of the material
+     */
     double getDensity() const;
 
+    /**
+     * @brief setDensity - Sets the density of the material
+     * @param density - The value set for the density of the material
+     */
     void setDensity(double density);
 
 };
